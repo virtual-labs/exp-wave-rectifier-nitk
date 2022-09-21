42a2300bf41b24aecee0c59b30069f740ebbfaaf
SOLVE_NITK <csd@nitk.edu.in>
2022-09-21 12:28:50 +0530

    Updated Experiments

---
+++ experiment/simulation/js/graph.js
@@ -0,0 +1,31 @@
+
+
+//  function plot_graph(pt,pty,context,truncate,gcolor,lwidth)
+// {
+// console.log(j);
+// ctx.save();
+
+// ctx.beginPath();
+// ctx.lineWidth=lwidth;
+// ctx.strokeStyle=gcolor;
+// ctx.moveTo(pt[1],pty[1]);
+// i=1;
+
+// while(i<pty.length)
+// {	
+// 	ctx.lineTo(pt[i],pty[i]);
+// 	i++;
+// 	if (i>=800)
+// 	{
+// 		pty.splice(0,1);
+// 		i=i-1;
+// 	}
+// }
+// ctx.stroke();
+// ctx.closePath();
+// ctx.restore();
+// }
+//  ptx.push(axcoord+j);
+// 	pty.push(aycoord);
+// 	j=j+1;
+// 		plot_graph(ptx,pty,ctx,1000);
