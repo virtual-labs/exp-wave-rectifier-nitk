1d316d414ca58dc5f1218c5d76e7f67091f7a9a6
SOLVE-NITK <csd@nitk.edu.in>
2024-10-28 12:15:20 +0530

    Simulation template updated to responsive one

--- experiment/simulation/js/sinewaves.js
+++ experiment/simulation/js/sinewaves.js
@@ -1,125 +1,125 @@
-//original
-function plotSine2(ctx, xOffset, yOffset,amplitude) {
-    console.log(amplitude)
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-   // var scale = 20;
+// //original
+// function plotSine2(ctx, xOffset, yOffset,amplitude) {
+//     console.log(amplitude)
+//     var width = ctx.canvas.width;
+//     var height = ctx.canvas.height;
+//    // var scale = 20;
     
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-     ctx.strokeStyle = "rgb(110,181,286)";
+//     ctx.beginPath();
+//     ctx.lineWidth = 3;
+//      ctx.strokeStyle = "rgb(110,181,286)";
     
-    // console.log("Drawing point...");
-    // drawPoint(ctx, yOffset+step);
+//     // console.log("Drawing point...");
+//     // drawPoint(ctx, yOffset+step);
     
-    let x = 0;
-    // let amplitude = 220;
+//     let x = 0;
+//     // let amplitude = 220;
     
-    let frequency = 50;
-    //ctx.moveTo(x, y);
-    ctx.moveTo(x, 100);
-    while (x < width) {
-        // y = height/2 + amplitude * Math.sin(w*(x+xOffset));
-            let mode =  Math.sin(w*(x+xOffset));
-                y = height/2 + amplitude * mode;
-                 ctx.lineTo(x, y);
-                 //ctx.lineTo(x,y1);
-                 // console.log("x="+x+" y="+y);
-                 ctx.stroke();
-                //  ctx.save();
-            x++;
+//     let frequency = 50;
+//     //ctx.moveTo(x, y);
+//     ctx.moveTo(x, 100);
+//     while (x < width) {
+//         // y = height/2 + amplitude * Math.sin(w*(x+xOffset));
+//             let mode =  Math.sin(w*(x+xOffset));
+//                 y = height/2 + amplitude * mode;
+//                  ctx.lineTo(x, y);
+//                  //ctx.lineTo(x,y1);
+//                  // console.log("x="+x+" y="+y);
+//                  ctx.stroke();
+//                 //  ctx.save();
+//             x++;
       
-    }
+//     }
    
    
-    // console.log("Drawing point at y=" + y);
-    ctx.stroke();
-    ctx.restore();
-}
+//     // console.log("Drawing point at y=" + y);
+//     ctx.stroke();
+//     ctx.restore();
+// }
 
-//clipped
-function plotSine2(ctx, xOffset, yOffset,amplitude) {
-    console.log(amplitude)
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-   // var scale = 20;
+// //clipped
+// function plotSine2(ctx, xOffset, yOffset,amplitude) {
+//     console.log(amplitude)
+//     var width = ctx.canvas.width;
+//     var height = ctx.canvas.height;
+//    // var scale = 20;
     
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-     ctx.strokeStyle = "rgb(110,181,286)";
+//     ctx.beginPath();
+//     ctx.lineWidth = 3;
+//      ctx.strokeStyle = "rgb(110,181,286)";
     
-    // console.log("Drawing point...");
-    // drawPoint(ctx, yOffset+step);
+//     // console.log("Drawing point...");
+//     // drawPoint(ctx, yOffset+step);
     
-    let x = 0;
-    // let amplitude = 220;
+//     let x = 0;
+//     // let amplitude = 220;
     
-    let frequency = 50;
-    //ctx.moveTo(x, y);
-    ctx.moveTo(x, 100);
-    while (x < width) {
-        // y = height/2 + amplitude * Math.sin(w*(x+xOffset));
-            let mode =  Math.sin(w*(x+xOffset));
-            // console.log(mode)
-            if(mode<0){
-                y = height/2 + amplitude * mode;
+//     let frequency = 50;
+//     //ctx.moveTo(x, y);
+//     ctx.moveTo(x, 100);
+//     while (x < width) {
+//         // y = height/2 + amplitude * Math.sin(w*(x+xOffset));
+//             let mode =  Math.sin(w*(x+xOffset));
+//             // console.log(mode)
+//             if(mode<0){
+//                 y = height/2 + amplitude * mode;
             
-                // y1 = height/2 + amplitude * Math.sin((w*(x+xOffset))-pi);
+//                 // y1 = height/2 + amplitude * Math.sin((w*(x+xOffset))-pi);
                  
-                 ctx.lineTo(x, y);
-                 //ctx.lineTo(x,y1);
-                 // console.log("x="+x+" y="+y);
-                 ctx.stroke();
-                //  ctx.save();
-            }  
-            x++;
+//                  ctx.lineTo(x, y);
+//                  //ctx.lineTo(x,y1);
+//                  // console.log("x="+x+" y="+y);
+//                  ctx.stroke();
+//                 //  ctx.save();
+//             }  
+//             x++;
       
-    }
+//     }
    
    
-    // console.log("Drawing point at y=" + y);
-    ctx.stroke();
-    ctx.restore();
-}
-//full wave
-function plotSine2(ctx, xOffset, yOffset,amplitude) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-   // var scale = 20;
+//     // console.log("Drawing point at y=" + y);
+//     ctx.stroke();
+//     ctx.restore();
+// }
+// //full wave
+// function plotSine2(ctx, xOffset, yOffset,amplitude) {
+//     var width = ctx.canvas.width;
+//     var height = ctx.canvas.height;
+//    // var scale = 20;
     
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-     ctx.strokeStyle = "rgb(110,181,286)";
+//     ctx.beginPath();
+//     ctx.lineWidth = 3;
+//      ctx.strokeStyle = "rgb(110,181,286)";
     
-    // console.log("Drawing point...");
-    // drawPoint(ctx, yOffset+step);
+//     // console.log("Drawing point...");
+//     // drawPoint(ctx, yOffset+step);
     
-    let x = 0;
-    // let amplitude = 220;
+//     let x = 0;
+//     // let amplitude = 220;
     
-    let frequency = 50;
-    //ctx.moveTo(x, y);
-    ctx.moveTo(x, 100);
-    while (x < width) {
-        // y = height/2 + amplitude * Math.sin(w*(x+xOffset));
-            let mode =  Math.sin(w*(x+xOffset));
-            let mode1 =  Math.sin((w*(x+xOffset))-Math.PI);
-            // console.log(mode)
+//     let frequency = 50;
+//     //ctx.moveTo(x, y);
+//     ctx.moveTo(x, 100);
+//     while (x < width) {
+//         // y = height/2 + amplitude * Math.sin(w*(x+xOffset));
+//             let mode =  Math.sin(w*(x+xOffset));
+//             let mode1 =  Math.sin((w*(x+xOffset))-Math.PI);
+//             // console.log(mode)
           
-            if(mode<0)
-                y = (height/2 + amplitude * mode);
-            else 
-            y = (height/2 + amplitude * mode1);
+//             if(mode<0)
+//                 y = (height/2 + amplitude * mode);
+//             else 
+//             y = (height/2 + amplitude * mode1);
 
-            ctx.lineTo(x, y);
+//             ctx.lineTo(x, y);
             
-                 ctx.stroke(); 
-            x++;
+//                  ctx.stroke(); 
+//             x++;
       
-    }
+//     }
    
    
-    // console.log("Drawing point at y=" + y);
-    ctx.stroke();
-    ctx.restore();
-}
+//     // console.log("Drawing point at y=" + y);
+//     ctx.stroke();
+//     ctx.restore();
+// }
